e2aecc2874c7788c323383e8f4c19c7cd0262015
gouflv <lv.gouf@gmail.com>
2021-08-04 15:17:46 +0800

    feat: Flatten nesting routes, and wrap component with TabConnector

--- src/router/config.ts
+++ src/router/config.ts
@@ -1,25 +1,50 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { RouteConfig } from 'vue-router'
 import BasicLayout from '../layouts/BasicLayout.vue'
+import BlankLayout from '../layouts/BlankLayout.vue'
 import About from '../views/About.vue'
 import Home from '../views/Home.vue'
-import { assignRoutes } from './utils'
+import { assignRoutes, flattenRoutesAndConnectToTab } from './utils'
 
-const mainRoute: Array<RouteConfig> = assignRoutes([
-  {
-    path: '/u',
-    component: BasicLayout,
-    children: [
-      { path: 'home', component: Home, meta: { title: 'Home' } },
-      { path: 'about', component: About, meta: { title: 'About' } },
-    ]
-  }
+const mainRoute: RouteConfig = {
+  path: '/',
+  component: BasicLayout,
+  children: [
+    {
+      path: 'example',
+      component: BlankLayout,
+      children: [
+        { path: 'home', component: Home, meta: { title: 'Home' } },
+        { path: 'about', component: About, meta: { title: 'About' } },
+      ]
+    },
+    { path: '', redirect: '/example/home' }
+  ]
+}
+
+/**
+ * 1. Setup: assign all routes
+ *
+ * 2. Flatten nesting routes, and wrap component with TabConnector
+ *
+ * Output:
+ * ```
+ * {
+ *   path: '/',
+ *   component: BasicLayout,
+ *   children: [
+ *     { path: '/example/home', name: 'ExampleHome', component: TabConnector(Home), meta },
+ *     { path: '/example/about', name: 'ExampleAbout', component: TabConnector(About), meta },
+ *   ]
+ * }
+ * ```
+ */
+const routes: Array<RouteConfig> = assignRoutes([
+  mainRoute
 ])
 
-const routes: Array<RouteConfig> = [
-  ...mainRoute,
-  { path: '', redirect: '/u/home' }
-]
+mainRoute.children = flattenRoutesAndConnectToTab(mainRoute.children!)
 
-console.log(mainRoute)
+console.log(routes)
 
 export { routes }
--- src/router/utils.ts
+++ src/router/utils.ts
@@ -1,17 +1,29 @@
 import { pascalCase } from 'change-case'
+import _ from 'lodash'
+import { join } from 'path'
+import Vue from 'vue'
 import { RouteConfig } from 'vue-router'
+import { Component, RouteConfigSingleView } from 'vue-router/types/router'
+import { tabService } from '../services/TabService'
 
 function pathToName(path: string) {
   return pascalCase(path.replace(/\//g, ''))
 }
 
+/**
+ * Compute some values and assign to the route config
+ */
 export function assignRoutes(routes: RouteConfig[]): RouteConfig[] {
 
   function dfs(routes: RouteConfig[], parent?: RouteConfig) {
     routes.forEach(route => {
-      // Generate route name
+      // Generate name
       const name = pathToName(route.path)
       route.name = parent ? `${parent.name}${name}` : name
+
+      // Generate fullPath
+      route.meta ??= {}
+      route.meta.fullPath = parent?.meta ? join(parent.meta.fullPath, route.path) : route.path
 
       if (route.children?.length) {
         dfs(route.children, route)
@@ -41,4 +53,47 @@ export const findRoute = (
 
   dfs(routes)
   return result
+}
+
+export function flattenRoutesAndConnectToTab(routes: RouteConfig[]): RouteConfig[] {
+  return _.reduce<RouteConfig, RouteConfig[]>(routes, (acc, route) => {
+    const { children } = route
+
+    if (!children?.length) {
+      acc.push(route)
+      return acc
+    }
+
+    children.forEach(child => {
+      const component = (child as RouteConfigSingleView).component
+      const meta = (child as RouteConfigSingleView).meta
+
+      if (!child.name || !component || !meta) {
+        throw new Error(`check route config ${child.path}`)
+      }
+
+      const connected = createTabConnector(child.name, component)
+
+      acc.push({
+        ...child,
+        path: meta.fullPath,
+        component: connected
+      })
+    })
+
+    return acc
+  }, [])
+}
+
+function createTabConnector (name: string, component: Component) {
+  return Vue.extend({
+    name,
+    render: h => {
+      const tab = tabService.find(name)
+
+      if (!tab) return h('div')
+
+      return h(component, { key: tab.timestamp })
+    }
+  })
 }
